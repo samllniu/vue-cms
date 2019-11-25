cd667e757a85c8be90b5a5a41b1bd7780cb6e08e
lijingwei <739097437@qq.com>
2019-11-25 20:32:30 +0800

    权限列表功能完成

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      <el-table :data="rightsList" stripe style="width: 100%">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="权限名称" prop="authName"></el-table-column>
+        <el-table-column label="路径" prop="path"></el-table-column>
+        <el-table-column label="权限等级" prop="level"></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 权限列表
+      rightsList: []
+    }
+  },
+  created() {
+    // 获取所有的权限
+    this.getRightsList()
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) {
+        return this.$$message.error('获取权限列表失败！')
+      }
+      this.rightsList = res.data
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
